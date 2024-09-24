af88b8dc2b7e5651d9379ce7018801d5bab37687
Abreu <abreu@disroot.org>
2024-09-24 11:28:34 -0300

    Deploy website - based on f0265dcaa763c05d81bb2803f689fccfed15f3df

---
+++ assets/js/3a47e627.daa2cb52.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[8460],{1189:e=>{e.exports=JSON.parse('{"categoryGeneratedIndex":{"title":"Sobre","description":"Texto descritivo","slug":"/category/sobre","permalink":"/docs/category/sobre","sidebar":"tutorialSidebar","navigation":{"next":{"title":"Pref\xe1cio","permalink":"/docs/sobre/prefacio"}}}}')}}]);
--- en/assets/js/dec18529.c1bff84e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[6340],{5574:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/be-a-ba/en/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
